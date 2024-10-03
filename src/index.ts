71a2629ee9d65565c15b7bfdbe3ad57265fc6505
Matt Bremmers <bremmdev@gmail.com>
2024-10-03 20:39:28 +0200

    test change

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono prod!')
+  return c.text('Hello Hono prod2!')
 })
 
 export default app
